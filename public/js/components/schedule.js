639fd069f07dc611ac78b45cc321ce2b6524e98b
zjjxj <592670776@qq.com>
2017-06-02 13:15:35 +0800

    commit

--- public/js/components/schedule.js
+++ public/js/components/schedule.js
@@ -2,12 +2,30 @@ import React from 'react';
 import {browserHistory} from 'react-router';
 
 
-export default class Schedule extends React.Component {
-    
+
+export default class Employee extends React.Component {
+
+
+
 
     render() {
+       
         return <div>
-            演出计划
+            <div>
+                <input type="text"/>
+                <button>查询</button>
+            </div>
+            <div>
+                <div>
+                    <span>ID</span>
+                    <span>影片名</span>
+                    <span>类型</span>
+                    <span>时长</span>
+                    <span>票价</span>
+                    <span>上映日期</span>
+                </div>
+            </div>
         </div>
     }
 }
+

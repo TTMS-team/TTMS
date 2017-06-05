c5b3bcec0406c7f8cedb5bcd43bf3a0c698e72f6
zjjxj <592670776@qq.com>
2017-06-05 18:04:24 +0800

    finish part of conductor functions

--- public/js/components/conductorPage.js
+++ public/js/components/conductorPage.js
@@ -2,29 +2,32 @@ require('../../css/conductorPageStyle.css');
 
 import React from 'react';
 import Title from './title';
-import {Link} from 'react-router';
+import {browserHistory} from 'react-router';
 
 export default class ConductorPage extends React.Component {
     componentWillMount() {
-        this.props.getFilms();
+        this.props.getPlayList();
     }
+    getSchedule(play_id){
+        browserHistory.push(`/schedulePage?id=${play_id}`);
 
+    }
 
     render() {
 
         //film:[{filmName:,duration,type,director,language}...]
         
 
-        var filmList=this.props.films.map((value,key)=>{
+        var playList=this.props.playList.map((value,key)=>{
             return <div >
-                <Link to={`/schedulePage?id=${value.play_id}&playName=${value.play_name}`}>
                     <div >
                         <span className="conductorSpan">{value.play_name}</span>
                         <span className="conductorSpan">{value.play_type_id}</span>
                         <span className="conductorSpan">{value.play_length}</span>
                         <span className="conductorSpan">{value.play_lang_id}</span>
+                        <span className="conductorSpan">{value.play_ticket_price}</span>
+                        <input type="button" value="演出计划" onClick={this.getSchedule.bind(this,value.play_id)}/>
                     </div>
-                </Link>
             </div>
         });
 
@@ -37,7 +40,14 @@ export default class ConductorPage extends React.Component {
                 <button className="searchButton">退票</button>
             </div>
             <div className="mainView1">
-                {filmList}
+                <div>
+                    <span>剧目名称</span>
+                    <span>剧目类型</span>
+                    <span>剧目时长</span>
+                    <span>剧目语言</span>
+                    <span>剧目票价</span>
+                </div>
+                {playList}
             </div>
         </div>
     }
--- public/js/components/schedulePage.js
+++ public/js/components/schedulePage.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Title from './title';
+import {browserHistory} from 'react-router';
 
 export default class SchedulePage extends React.Component {
-    componentWillMount() {
-        //play_id
-        this.props.getSchedule(this.props.location.query.id);
+    // componentWillMount() {
+    //     //根据play_id获取演出计划
+    //     this.props.getSchedule(this.props.location.query.id);
+    // }
+
+    getSeatPage(row,col){
+        browserHistory.push(`/seatPage?row=${row}&col=${col}`);
+
     }
-
-
     render() {
         var scheduleList=this.props.schedule.map((value,index)=>{
             return <div>
@@ -24,7 +28,9 @@ export default class SchedulePage extends React.Component {
         return <div>
             <Title username={this.props.username}/>
             <div>
-              <h2>{this.props.location.query.playName}</h2>
+                演出计划页面
+              <h2>{this.props.location.query.id}</h2>
+                <input type="button" value="选座" onClick={this.getSeatPage.bind(this,5,6)}/>
             </div>
             
         </div>
